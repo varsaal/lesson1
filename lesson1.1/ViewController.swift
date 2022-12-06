c52b4537ee7511930ea12dd1062ed3710ed6668e
varshaver <varshaveraleksey@gmail.com>
2022-12-06 13:29:02 +0300

    Initial Commit

---
+++ lesson1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson1.1
+//
+//  Created by 1234 on 06.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
