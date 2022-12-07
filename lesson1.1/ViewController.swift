cde3f86704db4f70cff23580ca94490ce6123a19
varshaver <varshaveraleksey@gmail.com>
2022-12-07 03:33:19 +0300

    1. Много изменений

--- lesson1.1/ViewController.swift
+++ lesson1.1/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWorldLabel: UILabel!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        startButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            startButton.setTitle("Clear text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            startButton.setTitle("Show text", for: .normal)
+        }
+        
+    }
+    
+   
 }
 
